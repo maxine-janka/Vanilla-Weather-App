ad39cdddb50cbd852e4bb57e25f069349ff04d3c
Maxine Janka <maxine.janka@gmail.com>
2024-02-26 18:51:14 +1100

    Added search function for city and current temperature using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#city-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "badact1734fb41b82663o4ab9a6e5204";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchCityInput = document.querySelector("#search-form-city-input");
-    let cityElement = document.querySelector("#weather-city");
-    cityElement.innerHTML = searchCityInput.value;
+    
+    searchCity(searchCityInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Melbourne");
