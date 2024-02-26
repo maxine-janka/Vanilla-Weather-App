4dfd8fe4684adca8e6aa6f202d71ffa923786612
Maxine Janka <maxine.janka@gmail.com>
2024-02-26 20:23:33 +1100

    Inserted more weather data from API

--- src/script.js
+++ src/script.js
@@ -2,8 +2,40 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#city-temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#weather-city");
+    let descriptionElement = document.querySelector("#weather-description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
+
+    timeElement.innerHTML = formatDate(date);
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
     temperatureElement.innerHTML = Math.round(temperature);
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/hr`;
+}
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+    	"Sunday", 
+      "Monday", 
+      "Tuesday", 
+      "Wednesday", 
+      "Thursady", 
+      "Friday", 
+      "Saturday"
+            ];
+
+    let day = days[date.getDay()];
+
+		if (minutes < 10) {
+			minutes = `0${minutes}`;
+		}
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
